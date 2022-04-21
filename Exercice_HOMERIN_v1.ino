8d41f82757d614c655048d7001e24076afda9e17
Emile Homerin <104089005+Emile59@users.noreply.github.com>
2022-04-21 19:24:18 +0200

    Update Exercice_HOMERIN_v1.ino
    Le code permet bien de réaliser l'application voulue avec affichage du résultat sur le port série.

--- Exercice_HOMERIN_v1.ino
+++ Exercice_HOMERIN_v1.ino
@@ -4,13 +4,14 @@
 
      - HOMERIN Emile
      - 21/04/2022
-     - Version 1 (sans matériel permettant de tester)
+     - Version 2 (Test concluant, l'application est fonctionnelle, il aura juste fallu
+                 définir le bouton poussoir en tant qu'entrée avec résistance de pullup)
 
      ---------------------------------------------------------------------------------- */
 
 
-#define LED 2          // Definition des pins correspondant à la LED et au bouton poussoir
-#define BUTTON 4
+#define LED 32          // Definition des pins correspondant à la LED et au bouton poussoir
+#define BUTTON 33
 
 int delay1, delay2;       // Durées aléatoires nécessaires à la réalisation de l'application
 int appui = 0;            // Vaut 0 dans le cas du 1er appui (lancement de l'application), 1 lorsqu'on détecte ensuite un front descendant (l'utilisateur appui sur 
@@ -22,7 +23,7 @@ void setup()
 {
   Serial.begin(115200);
 
-  pinMode(BUTTON, INPUT);     // Déclaration du bouton poussoir sur la pin 2
+  pinMode(BUTTON, INPUT_PULLUP);     // Déclaration du bouton poussoir sur la pin 2
   pinMode(LED, OUTPUT);       // Déclaration de la LED
 
   randomSeed(analogRead(0));    // Initialise le générateur de nombres aléatoires par le bruit aléatoire provenant de l'environnement
@@ -32,6 +33,7 @@ void setup()
 
 void loop()
 {
+     
   if ( digitalRead(BUTTON) == LOW &&  appui == 0)       // Si l'utilisateur appuie sur le bouton et que appui = 0 alors on lance l'application
   {
     delay1 = random(3000, 6001);   // Renvoie un nombre aléatoire entre 3000 et 6000
@@ -47,6 +49,7 @@ void loop()
     appui = 1;     // On attend à présent une action de l'utilisateur
   }
 
+     
   if ( digitalRead(BUTTON) == LOW  &&  appui == 1)       // Si l'utilisateur appuie sur le bouton et que appui = 1 alors on démarre le chronomètre
   {
     digitalWrite(LED, HIGH);        // Lorsque l'utilisateur appui sur le bouton pour reproduire le délai la LED S'allume
@@ -54,6 +57,7 @@ void loop()
     appui = 2;                      // Pour ne pas ré-initialiser le timer 
   }
 
+     
   if (digitalRead(BUTTON) == HIGH  &&  appui == 2)     // Si l'utilisateur relache le bouton et que appui = 2 alors on calcule delta
   {
     digitalWrite(LED, LOW);         // On éteind la LED
@@ -67,4 +71,5 @@ void loop()
     timer = 0;         // Réinitialisation du chronomètre
     appui = 0;         // Permet de relancer le processus lors d'un appui sur le bouton poussoir
   }
+     
 }
