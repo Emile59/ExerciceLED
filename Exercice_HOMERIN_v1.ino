0aa484745bdf842884e5a7583345894fb0c1ffa0
Emile Homerin <104089005+Emile59@users.noreply.github.com>
2022-04-22 09:36:23 +0200

    Update Exercice_HOMERIN_v1.ino
    Application finale

--- Exercice_HOMERIN_v1.ino
+++ Exercice_HOMERIN_v1.ino
@@ -3,20 +3,20 @@
      Mesure la différence entre un temps perçu et la reproduction de ce délai
 
      - HOMERIN Emile
-     - 21/04/2022
-     - Version 2 (Test concluant, l'application est fonctionnelle, il aura juste fallu
-                 définir le bouton poussoir en tant qu'entrée avec résistance de pullup)
+     - 22/04/2022
+     - Version 2 : Application fonctionnelle et affichage du résultat sur un écran LCD
 
      ---------------------------------------------------------------------------------- */
 
+#include <LiquidCrystal.h>   // Bibilothèque nécessaire pour utiliser l'écran LCD
 
 #define LED 32          // Definition des pins correspondant à la LED et au bouton poussoir
-#define BUTTON 33
+#define BUTTON 14
 
 int delay1, delay2;       // Durées aléatoires nécessaires à la réalisation de l'application
 int appui = 0;            // Vaut 0 dans le cas du 1er appui (lancement de l'application), 1 lorsqu'on détecte ensuite un front descendant (l'utilisateur appui sur 
                           // le bouton), puis 2 lorsqu'on detecte un front montant (l'utilisateur relache le bouton)
-int timer = 0, delta;     // timer sera notre chronomètre et delta représente la différence entre la durée de l'appui et le véritable temps d'allumage de la LED
+int timer, user_delay, delta;     // timer sera notre chronomètre, user_delay le temps d'appui de l'utilisateur et delta représente la différence entre la durée de l'appui et le véritable temps d'allumage de la LED
 
 
 void setup()
@@ -28,6 +28,8 @@ void setup()
 
   randomSeed(analogRead(0));    // Initialise le générateur de nombres aléatoires par le bruit aléatoire provenant de l'environnement
                                 // Ainsi la suite de nombres aléatoires démarre à un endroit arbitraire
+  
+  lcd.begin(16, 2);             // L’écran dispose de 16 colonnes et 2 lignes
 }
 
 
@@ -36,6 +38,10 @@ void loop()
      
   if ( digitalRead(BUTTON) == LOW &&  appui == 0)       // Si l'utilisateur appuie sur le bouton et que appui = 0 alors on lance l'application
   {
+    Serial.println("Nouvel essai");
+
+    lcd.clear();    // Efface le résultat précédent sur l'écran
+       
     delay1 = random(3000, 6001);   // Renvoie un nombre aléatoire entre 3000 et 6000
     delay2 = random(1000, 5001);   // Renvoie un nombre aléatoire entre 1000 et 5000
 
@@ -43,6 +49,10 @@ void loop()
 
     digitalWrite(LED, HIGH);    // Puis la LED s'allume
     delay(delay2);              // pendant 1 à 5 secondes aléatoirement
+       
+    Serial.print("Temps d'allumage de la LED : ");    // Permet de vérifier le bon fonctionnement de l'application
+    Serial.print(delay2);
+    Serial.println(" ms");
 
     digitalWrite(LED, LOW);     // Enfin la LED s'éteind à la fin du délai
 
@@ -61,12 +71,21 @@ void loop()
   if (digitalRead(BUTTON) == HIGH  &&  appui == 2)     // Si l'utilisateur relache le bouton et que appui = 2 alors on calcule delta
   {
     digitalWrite(LED, LOW);         // On éteind la LED
-    delta = abs(delay2 - (millis() - timer));    // Calcul de delta (on peut enlever la valeur absolue afin de connaitre le signe de l'erreur)
-                                                 // En effet millis() - timer correspond au temps de pression du bouton par l'utilisateur
+       
+    user_delay = millis() - timer;     // Calcul du temps d'appui de l'utilisateur
+       
+    Serial.print("Temps d'appui par l'utilisateur : ");    // Permet de vérifier le bon fonctionnement de l'application
+    Serial.print(user_delay);
+    Serial.println(" ms\n");
+       
+    delta = abs(delay2 - user_delay);    // Calcul de delta (on peut enlever la valeur absolue afin de connaitre le signe de l'erreur)
+                                         // En effet millis() - timer correspond au temps de pression du bouton par l'utilisateur
 
-    Serial.print(" Résultat : ");   // Affichage du résultat sur le port série
-    Serial.print(delta);
-    Serial.println(" ms \n");
+    lcd.setCursor(2,0);             // Affichage du résultat sur l'écran
+    lcd.print("Resultat : ");   
+    lcd.setCursor(3, 1);
+    lcd.print(delta);
+    lcd.print(" ms");
 
     timer = 0;         // Réinitialisation du chronomètre
     appui = 0;         // Permet de relancer le processus lors d'un appui sur le bouton poussoir
